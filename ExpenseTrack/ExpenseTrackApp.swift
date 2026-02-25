757cf54ea57a3e05e8ddbd5b97e7ac0b89b262d3
Faraz Amjad <farazamjad22@gmail.com>
2026-02-25 21:47:25 +0100

    Initial Commit

---
+++ ExpenseTrack/ExpenseTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackApp.swift
+//  ExpenseTrack
+//
+//  Created by Faraz Amjad on 25.02.26.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
